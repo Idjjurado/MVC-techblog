e69cb530fb32daa12081653b0b846a686a31adb2
idjjurado <ismael@eduphoria.net>
2023-07-11 12:26:42 -0600

    adding in user model

---
+++ models/User.js
@@ -0,0 +1,62 @@
+const { Model, DataTypes } = require("sequelize");
+const connection = require("../config/connection");
+const bcrypt = require("bcrypt");
+
+class User extends Model {
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [8],
+      },
+    },
+  },
+  {
+    hooks: {
+      beforeCreate: async (newUserData) => {
+        const plainTextPassword = newUserData.password;
+        const hashedPassword = await bcrypt.hash(plainTextPassword, 10);
+        newUserData.password = hashedPassword;
+        return newUserData;
+      },
+      beforeUpdate: async (updatedUserData) => {
+        const plainTextPassword = updatedUserData.password;
+        const hashedPassword = await bcrypt.hash(plainTextPassword, 10);
+        updatedUserData.password = hashedPassword;
+        return updatedUserData;
+      },
+    },
+    sequelize: connection,
+    timestamps: false,
+    freezeTableName: true,
+    modelName: "user",
+  }
+);
+
+module.exports = User;
